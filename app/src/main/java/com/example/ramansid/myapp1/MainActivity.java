8918af8023c3b48d3647a3ecda8006d416c4d7f8
fourier-being <being.fourier@gmail.com>
2018-09-03 18:40:40 -0700

    MainActivity Designv1.0 with BottomNavigationBar

--- app/src/main/java/com/example/ramansid/myapp1/MainActivity.java
+++ app/src/main/java/com/example/ramansid/myapp1/MainActivity.java
@@ -1,36 +1,38 @@
 package com.example.ramansid.myapp1;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.Toast;
-import android.text.TextUtils;
+import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     // imports
-    public Button writeBlog;
-    //public Button myBlogs;
-    //public Button browseBlogs;
-
+    private BottomNavigationView mBottomNavigation;
 
     //myBlogs =(Button)findViewById(R.id.myBlogs);
     //browseBlogs =(Button)findViewById(R.id.browseBlogs);
 
-    public void blogActivityInit(){
-        writeBlog =findViewById(R.id.startBlog);
-        writeBlog.setOnClickListener(new View.OnClickListener()
+    public void ActivityInit(){
+        mBottomNavigation = (BottomNavigationView)findViewById(R.id.navigationView);
+        mBottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener()
         {
             @Override
-            public void onClick(View view){
-            Intent intent = new Intent(MainActivity.this, BlogActivity.class);
-            startActivity(intent);
+            public boolean onNavigationItemSelected(@NonNull MenuItem item){
+                Fragment frag = null;
+                switch(item.getItemId()) {
+                    case R.id.startNewBlog:
+                        Intent intent = new Intent(MainActivity.this, BlogActivity.class);
+                        startActivity(intent);
+                }
+                return true;
     }
     });
 }
@@ -38,6 +40,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        blogActivityInit();
+        ActivityInit();
     }
 }
